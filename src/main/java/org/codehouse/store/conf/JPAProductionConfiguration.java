76265647f923ca0cce2cea8d2cf6a947e8d8f686
Joao Felipe de Medeiros Moreira <jfmedeiros1820@gmail.com>
2016-12-08 15:09:09 -0200

    Injection missing

--- src/main/java/org/codehouse/store/conf/JPAProductionConfiguration.java
+++ src/main/java/org/codehouse/store/conf/JPAProductionConfiguration.java
@@ -1,5 +1,6 @@
 package org.codehouse.store.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
@@ -11,12 +12,13 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-@Profile("prod")
 public class JPAProductionConfiguration {
 
+  @Autowired
   private Environment environment;
 
   @Bean
+  @Profile("prod")
   public DataSource dataSource() throws URISyntaxException {
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
     dataSource.setDriverClassName("org.postgresql.Driver");
@@ -31,6 +33,7 @@ public class JPAProductionConfiguration {
   }
 
   @Bean
+  @Profile("prod")
   private Properties aditionalProperties() {
     Properties props = new Properties();
     props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
